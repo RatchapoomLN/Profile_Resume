09d3600e77adbcd9a1990acf45eb1b0762ff2b2d
RatchapoomLN <longnight2543@gmail.com>
2021-06-16 10:36:18 +0700

    first commit

---
+++ home.js
@@ -0,0 +1,16 @@
+{var target = document.querySelector("footer");
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement
+function callback(entries, observer) {
+  scrollToTopBtn.classList.add("showBtn");
+}
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+let observer = new IntersectionObserver(callback);
+observer.observe(target);
+}
