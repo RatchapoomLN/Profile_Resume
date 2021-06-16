bc6a7d2c78e11de28f3a43b7e010715bb8361e93
RatchapoomLN <longnight2543@gmail.com>
2021-06-16 10:57:01 +0700

    arrange

--- home.js
+++ home.js
@@ -1,16 +1,17 @@
-{var target = document.querySelector("footer");
-var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
-var rootElement = document.documentElement
-function callback(entries, observer) {
-  scrollToTopBtn.classList.add("showBtn");
-}
-function scrollToTop() {
-  rootElement.scrollTo({
-    top: 0,
-    behavior: "smooth"
-  })
-}
-scrollToTopBtn.addEventListener("click", scrollToTop);
-let observer = new IntersectionObserver(callback);
-observer.observe(target);
+{
+  var target = document.querySelector("footer");
+  var scrollToTopBtn = document.querySelector(".scrollToTopBtn");
+  var rootElement = document.documentElement;
+  function callback(entries, observer) {
+    scrollToTopBtn.classList.add("showBtn");
+  }
+  function scrollToTop() {
+    rootElement.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+  scrollToTopBtn.addEventListener("click", scrollToTop);
+  let observer = new IntersectionObserver(callback);
+  observer.observe(target);
 }
